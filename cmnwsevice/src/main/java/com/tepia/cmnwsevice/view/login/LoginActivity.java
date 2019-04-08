e71528967f3747e96bc7b96c1f6eeda54ea939a5
张鑫华-Android开发-武汉 <137330275@qq.com>
2019-04-08 10:47:10 +0800

    登录接口

--- cmnwsevice/src/main/java/com/tepia/cmnwsevice/view/login/LoginActivity.java
+++ cmnwsevice/src/main/java/com/tepia/cmnwsevice/view/login/LoginActivity.java
@@ -7,6 +7,7 @@ import android.databinding.ViewDataBinding;
 import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.tepia.base.AppRoutePath;
 import com.tepia.base.mvp.MVPBaseActivity;
 import com.tepia.base.utils.DoubleClickUtil;
@@ -15,13 +16,13 @@ import com.tepia.cmnwsevice.databinding.ActivityLoginBinding;
 
 
 /**
- * @author        :       zhang xinhua
- * @Version       :       1.0
- * @创建人         ：      zhang xinhua
- * @创建时间       :       2019/4/2 15:41
- * @修改人         ：
- * @修改时间       :       2019/4/2 15:41
- * @功能描述       :        登录页面
+ * @author :       zhang xinhua
+ * @Version :       1.0
+ * @创建人 ：      zhang xinhua
+ * @创建时间 :       2019/4/2 15:41
+ * @修改人 ：
+ * @修改时间 :       2019/4/2 15:41
+ * @功能描述 :        登录页面
  **/
 @Route(path = AppRoutePath.app_cmnw_login)
 public class LoginActivity extends MVPBaseActivity<LoginContract.View, LoginPresenter> implements LoginContract.View {
@@ -35,7 +36,7 @@ public class LoginActivity extends MVPBaseActivity<LoginContract.View, LoginPres
 
     @Override
     public void initView() {
-       mBinding =  DataBindingUtil.bind(mRootView);
+        mBinding = DataBindingUtil.bind(mRootView);
     }
 
     @Override
@@ -48,10 +49,10 @@ public class LoginActivity extends MVPBaseActivity<LoginContract.View, LoginPres
         mBinding.tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (DoubleClickUtil.isFastDoubleClick()){
+                if (DoubleClickUtil.isFastDoubleClick()) {
                     return;
                 }
-                mPresenter.login("username","admin","password","123456");
+                mPresenter.login("username", "admin", "password", "123456");
             }
         });
     }
@@ -62,6 +63,6 @@ public class LoginActivity extends MVPBaseActivity<LoginContract.View, LoginPres
 
     @Override
     public void loginSuccess() {
-
+        ARouter.getInstance().build(AppRoutePath.app_cmnw_activity_tabmain).navigation();
     }
 }
