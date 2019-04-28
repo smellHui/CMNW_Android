80154bf8fddc4652185ce28b34f274f8ee6f7779
xch <252243862@qq.com>
2019-04-28 10:44:43 +0800

    反馈文档bug9-顶端的文字被刘海屏遮挡

--- cmdbsevice/src/main/java/com/tepia/cmdbsevice/util/UiHelper.java
+++ cmdbsevice/src/main/java/com/tepia/cmdbsevice/util/UiHelper.java
@@ -12,6 +12,12 @@ import com.tepia.cmdbsevice.view.alarmstatistics.AlarmStatisticsActivity;
  */
 public class UiHelper {
 
+    /**
+     * 事件督办列表页
+     *
+     * @param ctx
+     * @param tabIndex
+     */
     public static void goToAlarmStatisticsView(Context ctx, int tabIndex) {
         Intent intent = new Intent(ctx, AlarmStatisticsActivity.class);
         intent.putExtra("tabIndex", tabIndex);
